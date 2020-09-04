a49ffdc99e474065017f1c64e46982d237002387
Sebastián Marroquín Martínez <sebasmarro10@gmail.com>
2020-09-04 12:16:55 -0500

    Add files via upload

---
+++ 1erParcial/Practica6_Marroquin/Practica6_Marroquin/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿using System;
+
+namespace Practica6_Marroquin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Firma = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.label29 = new System.Windows.Forms.Label();
+            this.label30 = new System.Windows.Forms.Label();
+            this.label34 = new System.Windows.Forms.Label();
+            this.label35 = new System.Windows.Forms.Label();
+            this.label36 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Humnst777 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALCALDIAS DE LA CIUDAD DE MEXICO";
+            // 
+            // Firma
+            // 
+            this.Firma.BackColor = System.Drawing.Color.Transparent;
+            this.Firma.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Firma.BackgroundImage")));
+            this.Firma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Firma.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Firma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Firma.Font = new System.Drawing.Font("Humnst777 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Firma.Location = new System.Drawing.Point(310, 500);
+            this.Firma.Name = "Firma";
+            this.Firma.Size = new System.Drawing.Size(49, 50);
+            this.Firma.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.Firma, "Firma del Desarrollador");
+            this.Firma.UseVisualStyleBackColor = false;
+            this.Firma.Click += new System.EventHandler(this.Firma_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.Color.Transparent;
+            this.label17.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label17.Location = new System.Drawing.Point(297, 179);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(13, 13);
+            this.label17.TabIndex = 19;
+            this.label17.Text = "1";
+            this.toolTip1.SetToolTip(this.label17, "Azcapotzalco");
+            this.label17.Click += new System.EventHandler(this.label17_Click);
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.BackColor = System.Drawing.Color.Transparent;
+            this.label19.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label19.Location = new System.Drawing.Point(333, 179);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(13, 13);
+            this.label19.TabIndex = 20;
+            this.label19.Text = "2";
+            this.toolTip1.SetToolTip(this.label19, "Gustavo A. Madero");
+            this.label19.Click += new System.EventHandler(this.label19_Click);
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.BackColor = System.Drawing.Color.Transparent;
+            this.label20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label20.Location = new System.Drawing.Point(287, 209);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(13, 13);
+            this.label20.TabIndex = 21;
+            this.label20.Text = "3";
+            this.toolTip1.SetToolTip(this.label20, "Miguel Hidalgo");
+            this.label20.Click += new System.EventHandler(this.label20_Click);
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.BackColor = System.Drawing.Color.Transparent;
+            this.label21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label21.Location = new System.Drawing.Point(314, 212);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(13, 13);
+            this.label21.TabIndex = 22;
+            this.label21.Text = "4";
+            this.toolTip1.SetToolTip(this.label21, "Cuahutémoc");
+            this.label21.Click += new System.EventHandler(this.label21_Click);
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.BackColor = System.Drawing.Color.Transparent;
+            this.label22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label22.Location = new System.Drawing.Point(346, 212);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(13, 13);
+            this.label22.TabIndex = 23;
+            this.label22.Text = "5";
+            this.toolTip1.SetToolTip(this.label22, "Venustiano Carranza");
+            this.label22.Click += new System.EventHandler(this.label22_Click);
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.BackColor = System.Drawing.Color.Transparent;
+            this.label23.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label23.Location = new System.Drawing.Point(226, 262);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(13, 13);
+            this.label23.TabIndex = 24;
+            this.label23.Text = "6";
+            this.toolTip1.SetToolTip(this.label23, "Cuajimalpa de Morelos");
+            this.label23.Click += new System.EventHandler(this.label23_Click);
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.BackColor = System.Drawing.Color.Transparent;
+            this.label24.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label24.Location = new System.Drawing.Point(268, 252);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(13, 13);
+            this.label24.TabIndex = 25;
+            this.label24.Text = "7";
+            this.toolTip1.SetToolTip(this.label24, "Álvaro Obregón");
+            this.label24.Click += new System.EventHandler(this.label24_Click);
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.BackColor = System.Drawing.Color.Transparent;
+            this.label25.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label25.Location = new System.Drawing.Point(307, 240);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(13, 13);
+            this.label25.TabIndex = 26;
+            this.label25.Text = "8";
+            this.toolTip1.SetToolTip(this.label25, "Benito Juárez ");
+            this.label25.Click += new System.EventHandler(this.label25_Click);
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.BackColor = System.Drawing.Color.Transparent;
+            this.label26.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label26.Location = new System.Drawing.Point(337, 233);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(13, 13);
+            this.label26.TabIndex = 27;
+            this.label26.Text = "9";
+            this.toolTip1.SetToolTip(this.label26, "Iztacalco");
+            this.label26.Click += new System.EventHandler(this.label26_Click);
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.BackColor = System.Drawing.Color.Transparent;
+            this.label27.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label27.Location = new System.Drawing.Point(241, 320);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(19, 13);
+            this.label27.TabIndex = 28;
+            this.label27.Text = "10";
+            this.toolTip1.SetToolTip(this.label27, "La Magdalena Contreras");
+            this.label27.Click += new System.EventHandler(this.label27_Click);
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.BackColor = System.Drawing.Color.Transparent;
+            this.label28.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label28.Location = new System.Drawing.Point(311, 274);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(19, 13);
+            this.label28.TabIndex = 29;
+            this.label28.Text = "11";
+            this.toolTip1.SetToolTip(this.label28, "Coyoacán");
+            this.label28.Click += new System.EventHandler(this.label28_Click);
+            // 
+            // label29
+            // 
+            this.label29.AutoSize = true;
+            this.label29.BackColor = System.Drawing.Color.Transparent;
+            this.label29.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label29.Location = new System.Drawing.Point(364, 263);
+            this.label29.Name = "label29";
+            this.label29.Size = new System.Drawing.Size(19, 13);
+            this.label29.TabIndex = 30;
+            this.label29.Text = "12";
+            this.toolTip1.SetToolTip(this.label29, "Iztapalapa");
+            this.label29.Click += new System.EventHandler(this.label29_Click);
+            // 
+            // label30
+            // 
+            this.label30.AutoSize = true;
+            this.label30.BackColor = System.Drawing.Color.Transparent;
+            this.label30.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label30.Location = new System.Drawing.Point(396, 301);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(19, 13);
+            this.label30.TabIndex = 31;
+            this.label30.Text = "15";
+            this.toolTip1.SetToolTip(this.label30, "Tláhuac");
+            this.label30.Click += new System.EventHandler(this.label30_Click);
+            // 
+            // label34
+            // 
+            this.label34.AutoSize = true;
+            this.label34.BackColor = System.Drawing.Color.Transparent;
+            this.label34.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label34.Location = new System.Drawing.Point(346, 320);
+            this.label34.Name = "label34";
+            this.label34.Size = new System.Drawing.Size(19, 13);
+            this.label34.TabIndex = 35;
+            this.label34.Text = "14";
+            this.toolTip1.SetToolTip(this.label34, "Xochimilco");
+            this.label34.Click += new System.EventHandler(this.label34_Click);
+            // 
+            // label35
+            // 
+            this.label35.AutoSize = true;
+            this.label35.BackColor = System.Drawing.Color.Transparent;
+            this.label35.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label35.Location = new System.Drawing.Point(278, 354);
+            this.label35.Name = "label35";
+            this.label35.Size = new System.Drawing.Size(19, 13);
+            this.label35.TabIndex = 36;
+            this.label35.Text = "13";
+            this.toolTip1.SetToolTip(this.label35, "Tlalpan");
+            this.label35.Click += new System.EventHandler(this.label35_Click);
+            // 
+            // label36
+            // 
+            this.label36.AutoSize = true;
+            this.label36.BackColor = System.Drawing.Color.Transparent;
+            this.label36.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label36.Location = new System.Drawing.Point(369, 384);
+            this.label36.Name = "label36";
+            this.label36.Size = new System.Drawing.Size(19, 13);
+            this.label36.TabIndex = 37;
+            this.label36.Text = "16";
+            this.toolTip1.SetToolTip(this.label36, "Milpa Alta");
+            this.label36.Click += new System.EventHandler(this.label36_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Humnst777 BlkCn BT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(412, 19);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Navegue a traves de los números del mapa para conocer más. ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(644, 562);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label36);
+            this.Controls.Add(this.label35);
+            this.Controls.Add(this.label34);
+            this.Controls.Add(this.label30);
+            this.Controls.Add(this.label29);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.Firma);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ALCALDIAS CDMX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Firma;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label29;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label34;
+        private System.Windows.Forms.Label label35;
+        private System.Windows.Forms.Label label36;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 1erParcial/Practica6_Marroquin/Practica6_Marroquin/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica6_Marroquin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Firma_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Practica2_Marroquin.exe");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Azcapotzalco.pdf");
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("GustavoAMadero.pdf");
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("MiguelHidalgo.pdf");
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Cuahutemoc.pdf");
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("VenustianoCarranza.pdf");
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Cuajimalpa.pdf");
+        }
+
+        private void label24_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("AlvaroObregon.pdf");
+        }
+
+        private void label25_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("BenitoJuarez.pdf");
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Iztacalco.pdf");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("MagdalenaContreras.pdf");
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label28_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Coyoacan.pdf");
+        }
+
+        private void label27_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("MagdalenaContreras.pdf");
+        }
+
+        private void label29_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Iztapalapa.pdf");
+        }
+
+        private void label35_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Tlalpan.pdf");
+        }
+
+        private void label34_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Xochimilco.pdf");
+        }
+
+        private void label30_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Tlahuac.pdf");
+        }
+
+        private void label36_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("MilpaAlta.pdf");
+        }
+    }
+}
