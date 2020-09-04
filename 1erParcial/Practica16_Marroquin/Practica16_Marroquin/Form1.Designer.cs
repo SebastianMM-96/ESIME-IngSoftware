cfbe27eafa04075974ddc99c980ccc0deda2d892
Sebastián Marroquín Martínez <sebasmarro10@gmail.com>
2020-09-04 12:23:13 -0500

    Add files via upload

---
+++ 1erParcial/Practica16_Marroquin/Practica16_Marroquin/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Practica16_Marroquin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_fx_valor1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_fx = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.funcion = new System.Windows.Forms.Button();
+            this.listBox_Result_Function = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirDeLaAplicaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.firmaDelDesarrolladorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Humanst521 BT", 13F);
+            this.label9.Location = new System.Drawing.Point(38, 170);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(144, 21);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Ingrese la Función";
+            // 
+            // textBox_fx_valor1
+            // 
+            this.textBox_fx_valor1.Location = new System.Drawing.Point(42, 127);
+            this.textBox_fx_valor1.Name = "textBox_fx_valor1";
+            this.textBox_fx_valor1.Size = new System.Drawing.Size(225, 20);
+            this.textBox_fx_valor1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Humanst521 BT", 13F);
+            this.label6.Location = new System.Drawing.Point(42, 94);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(164, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ingrese el Valor de x";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Humanst521 BT", 13F);
+            this.label1.Location = new System.Drawing.Point(38, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "f(x)";
+            // 
+            // textBox_fx
+            // 
+            this.textBox_fx.Location = new System.Drawing.Point(80, 202);
+            this.textBox_fx.Name = "textBox_fx";
+            this.textBox_fx.Size = new System.Drawing.Size(187, 20);
+            this.textBox_fx.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Humanst521 Lt BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(393, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(173, 44);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Para calcular la función\r\npresione el boton";
+            // 
+            // funcion
+            // 
+            this.funcion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.funcion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.funcion.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.funcion.Location = new System.Drawing.Point(397, 158);
+            this.funcion.Name = "funcion";
+            this.funcion.Size = new System.Drawing.Size(169, 33);
+            this.funcion.TabIndex = 6;
+            this.funcion.Text = "f(x)";
+            this.funcion.UseVisualStyleBackColor = true;
+            this.funcion.Click += new System.EventHandler(this.funcion_Click);
+            // 
+            // listBox_Result_Function
+            // 
+            this.listBox_Result_Function.FormattingEnabled = true;
+            this.listBox_Result_Function.Location = new System.Drawing.Point(656, 94);
+            this.listBox_Result_Function.Name = "listBox_Result_Function";
+            this.listBox_Result_Function.Size = new System.Drawing.Size(304, 329);
+            this.listBox_Result_Function.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Humanst521 BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Calculadora de Funciones";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.informaciónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1014, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirDeLaAplicaciónToolStripMenuItem});
+            this.archivoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("archivoToolStripMenuItem.Image")));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirDeLaAplicaciónToolStripMenuItem
+            // 
+            this.salirDeLaAplicaciónToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("salirDeLaAplicaciónToolStripMenuItem.Image")));
+            this.salirDeLaAplicaciónToolStripMenuItem.Name = "salirDeLaAplicaciónToolStripMenuItem";
+            this.salirDeLaAplicaciónToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.salirDeLaAplicaciónToolStripMenuItem.Text = "Salir de la Aplicación";
+            this.salirDeLaAplicaciónToolStripMenuItem.Click += new System.EventHandler(this.salirDeLaAplicaciónToolStripMenuItem_Click);
+            // 
+            // informaciónToolStripMenuItem
+            // 
+            this.informaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.firmaDelDesarrolladorToolStripMenuItem});
+            this.informaciónToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("informaciónToolStripMenuItem.Image")));
+            this.informaciónToolStripMenuItem.Name = "informaciónToolStripMenuItem";
+            this.informaciónToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.informaciónToolStripMenuItem.Text = "Información ";
+            // 
+            // firmaDelDesarrolladorToolStripMenuItem
+            // 
+            this.firmaDelDesarrolladorToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("firmaDelDesarrolladorToolStripMenuItem.Image")));
+            this.firmaDelDesarrolladorToolStripMenuItem.Name = "firmaDelDesarrolladorToolStripMenuItem";
+            this.firmaDelDesarrolladorToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.firmaDelDesarrolladorToolStripMenuItem.Text = "Firma del Desarrollador";
+            this.firmaDelDesarrolladorToolStripMenuItem.Click += new System.EventHandler(this.firmaDelDesarrolladorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 431);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_fx_valor1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_fx);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.funcion);
+            this.Controls.Add(this.listBox_Result_Function);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Calculadora de Funciones";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_fx_valor1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_fx;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button funcion;
+        private System.Windows.Forms.ListBox listBox_Result_Function;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirDeLaAplicaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem firmaDelDesarrolladorToolStripMenuItem;
+    }
+}
+
---
+++ 1erParcial/Practica16_Marroquin/Practica16_Marroquin/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using info.lundin.math;
+
+
+namespace Practica16_Marroquin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void funcion_Click(object sender, EventArgs e)
+        {
+            String fx;
+            double x, r;
+            x = double.Parse(textBox_fx_valor1.Text);
+            fx = textBox_fx.Text;
+            r = fu(x, fx);
+            listBox_Result_Function.Items.Add(r);
+        }
+
+        double fu(double x, String fx)
+        {
+            double r;
+            ExpressionParser ec1 = new ExpressionParser();
+            ec1.Values.Add("x", x);
+            r = ec1.Parse(fx);
+            return (r);
+
+        }
+
+        private void salirDeLaAplicaciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void firmaDelDesarrolladorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Firma del alumno. 
+            System.Diagnostics.Process.Start("Practica2_Marroquin.exe");
+        }
+    }
+}
