4b02501ef73ce087431ff6f2c7343f30101739b9
Sebastián Marroquín Martínez <sebasmarro10@gmail.com>
2020-09-04 12:19:49 -0500

    Add files via upload

---
+++ 1erParcial/Practica5_Marroquin/Practica5_Marroquin/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica5_Marroquin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chihuahua.txt");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Sonora.txt");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Coahuila.txt");
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Guerrero.txt");
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Zacatecas.txt");
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Nayarit.txt");
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Queretaro.txt");
+        }
+
+        private void label30_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("CDMX.txt");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Practica2_Marroquin.exe");
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("BajaCalifornia.txt");
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("BajaCalifornia Sur.txt");
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Sinaloa.txt");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Durango.txt");
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Nuevo Leon.txt");
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("SanLuisPotosi.txt");
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Tamaulipas.txt");
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Jalisco.txt");
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Colima.txt");
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Michoacan.txt");
+        }
+
+        private void label24_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Puebla.txt");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Oaxaca.txt");
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chiapas.txt");
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Tabasco.txt");
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Campeche.txt");
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("QuintanaRoo.txt");
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Yucatan.txt");
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Veracruz.txt");
+        }
+
+        private void label27_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("EstadoDeMexico.txt");
+        }
+
+        private void label25_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Aguascalientes.txt");
+        }
+
+        private void label28_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Hidalgo.txt");
+        }
+
+        private void label29_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Morelos.txt");
+        }
+
+        private void label31_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Guanajuato.txt");
+        }
+
+        private void label32_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Tlaxcala.txt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
